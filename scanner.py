e52fcc0f13d208d6438547f7713722a172d86cab
kaydee001 <kartik.dhasmana001@gmail.com>
2026-04-16 11:11:39 +0530

    .txt report generator and uppercase detection to naming checker

--- scanner.py
+++ scanner.py
@@ -32,25 +32,28 @@ def name_checker(assets):
     err_name = []
     for asset in assets:
         file_name, file_ext = os.path.splitext(asset)
-        if " " in file_name:
+        if " " in file_name or any(c.isupper() for c in os.path.basename(file_name)):
             err_name.append(file_name+file_ext)
     return err_name
 
 
+def generate_report(scanned_assets, asset_count, check_name):
+    with open("report.txt", "w") as file:
+        print(f"Asset Pipeline Report", file=file)
+        print("="*20, file=file)
+        print(f"total assets found {len(scanned_assets)} : ", file=file)
+        for key, value in asset_count.items():
+            print(f"-{key} : {value}", file=file)
+        print(f"naming issues : {len(check_name)}", file=file)
+        for asset in check_name:
+            print(
+                f"- {os.path.basename(asset)}", file=file)
+        file.write("="*20)
+
+
 if __name__ == "__main__":
     scanned_assets = scan_assets(ROOT_DIR, ALLOWED_EXTENSIONS)
-
-    print(f"Asset Pipeline Report")
-    print("="*20)
     asset_count = count_assets(scanned_assets)
-    print(f"total assets found {len(scanned_assets)} : ")
-
-    for key, value in asset_count.items():
-        print(f" -{key} : ", value)
-
     check_name = name_checker(scanned_assets)
-    print(f"naming issues : {len(check_name)}")
-    for asset in check_name:
-        print(
-            f" - {os.path.basename(asset)}")
-    print("="*20)
+
+    generate_report(scanned_assets, asset_count, check_name)
